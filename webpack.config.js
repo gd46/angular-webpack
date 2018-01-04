426ec977e5c358c12d4700516e5fa5eb4736b64c
DiBella, Giuseppe <Giuseppe.DiBella@ADP.com>
2018-01-03 19:06:12 -0500

    fix: remove target node and exclude nodeExternals as that is for backend config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const { ContextReplacementPlugin } = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  target: 'node',
+  // target: 'node', // For backend app
   context: __dirname + '/src',
   entry: {
     main: './main.ts'
@@ -21,21 +21,31 @@ module.exports = {
     extensions: ['.js', '.ts', '.html']
   },
   devServer: {
-    contentBase: path.join(__dirname, "../build/"),
+    contentBase: path.join(__dirname, '../build/'),
     port: 9000
   },
-  devtool: 'inline-source-map',
-  externals: [nodeExternals()],
+  devtool: 'source-map',
+  // externals: [nodeExternals()], // For backend app
   module: {
     loaders: [
-      { test: /.ts$/, use: ['awesome-typescript-loader', 'angular2-template-loader'] },
-      { test: /.html$/, use: 'raw-loader' }
+      {
+        test: /.ts$/,
+        use: [
+          'awesome-typescript-loader',
+          'angular2-template-loader'
+        ]
+      },
+      {
+        test: /.html$/,
+        use: 'raw-loader'
+      }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(['build']),
     new HtmlWebpackPlugin({
-      template: './index.html' // Dynamically includes bundles to index.html
+      template: './index.html', // Dynamically includes bundles to index.html
+      title: 'Webpack App'
     }),
     // new BundleAnalyzerPlugin(), Output bundle size info
     // new webpack.optimize.CommonsChunkPlugin({

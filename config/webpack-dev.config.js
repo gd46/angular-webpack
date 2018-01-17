eba774cdda8d897b11f5b94937dde89147d6dd6d
DiBella, Giuseppe <Giuseppe.DiBella@ADP.com>
2018-01-17 18:45:08 -0500

    build: Update webpack config to take advantage of env

--- config/webpack-dev.config.js
+++ config/webpack-dev.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const { ContextReplacementPlugin } = require('webpack');
+const {ContextReplacementPlugin} = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -8,64 +8,68 @@ const {TsConfigPathsPlugin} = require('awesome-typescript-loader');
 
 let conditionalPlugins = [];
 
-if(process.env.BUNDLEANALYZER) {
-  conditionalPlugins.push(new BundleAnalyzerPlugin());
-}
+module.exports = (env) => {
 
-module.exports = {
-  context: path.resolve(__dirname, '../src'),
-  entry: {
-    main: './main.ts',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '../build')
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.html'],
+  if(env.BUNDLEANALYZER) {
+    conditionalPlugins.push(new BundleAnalyzerPlugin());
+  }
+  
+  return {
+
+    context: path.resolve(__dirname, '../src'),
+    entry: {
+      main: './main.ts',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, '../build')
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.html'],
+      plugins: [
+        new TsConfigPathsPlugin({configFileName: '../tsconfig.json'})
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, '../build/'),
+      port: 9000
+    },
+    devtool: 'inline-source-map',
+    module: {
+      loaders: [
+        {
+          test: /.ts$/,
+          use: [
+            'awesome-typescript-loader',
+            'angular2-template-loader'
+          ]
+        },
+        {
+          test: /.html$/,
+          use: 'raw-loader'
+        },
+        {
+          test: /\.scss$/,
+          use: ['raw-loader', 'sass-loader']
+        }
+      ]
+    },
     plugins: [
-      new TsConfigPathsPlugin({ configFileName: '../tsconfig.json'})
+      ...conditionalPlugins,
+      new CleanWebpackPlugin(['build'], {
+        root: path.resolve(__dirname, '../')
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html', // Dynamically includes bundles to index.html
+        title: 'Webpack App'
+      }),
+      // new webpack.optimize.CommonsChunkPlugin({
+      //   name: 'vendor'
+      // }),
+      new ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)@angular/,
+        path.resolve(__dirname, '../src')
+      ),
     ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, '../build/'),
-    port: 9000
-  },
-  devtool: 'inline-source-map',
-  module: {
-    loaders: [
-      {
-        test: /.ts$/,
-        use: [
-          'awesome-typescript-loader',
-          'angular2-template-loader'
-        ]
-      },
-      {
-        test: /.html$/,
-        use: 'raw-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ['raw-loader', 'sass-loader']
-      }
-    ]
-  },
-  plugins: [
-    ...conditionalPlugins,
-    new CleanWebpackPlugin(['build'], {
-      root: path.resolve(__dirname, '../')
-    }),
-    new HtmlWebpackPlugin({
-      template: './index.html', // Dynamically includes bundles to index.html
-      title: 'Webpack App'
-    }),
-    // new webpack.optimize.CommonsChunkPlugin({
-    //   name: 'vendor'
-    // }),
-    new ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)@angular/,
-      path.resolve(__dirname, '../src')
-    ),
-  ]
-};
+  };
+}

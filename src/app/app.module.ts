599cb6d1d823d303cee9bd2fac4764f28727ec3d
DiBella, Giuseppe <Giuseppe.DiBella@ADP.com>
2018-01-22 19:06:36 -0500

    refactor: Update folder structure under src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+
 
 @NgModule({
   declarations: [
